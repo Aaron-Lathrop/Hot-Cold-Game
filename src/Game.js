2978b4ff13bc8a914477e13ef65bc59540d24bd0
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-11-24 18:06:51 -0700

    first commit

---
+++ src/Game.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            guessList: [],
+            secretNumber: Math.floor(Math.random() * 100)
+        }
+    }
+
+    newGame() {
+        this.setState({secretNumber: Math.floor(Math.random() * 100)});
+    }
+
+    giveFeedback(value) {
+        let feedback = "";
+        const secretNumber = this.state.secretNumber;
+        const difference = Math.abs(secretNumber - value);
+        if(value === secretNumber) {
+                feedback = "Winner winner chicken dinner!!!";
+            } else if(value === undefined || value > 100 || value < 0) {
+                feedback = "Please guess a number between 0 and 100";
+            } else if(difference< 10){
+                feedback = "Hot";
+            } else if(difference > 9 && difference < 20) {
+                feedback = "Kinda hot";
+            } else if(difference > 19 && difference < 30){
+                feedback = "Luke warm";
+            } else {
+                feedback = "Cold";
+            }
+        return feedback;
+    }
+
+    addGuess(guess) {
+        this.setState({...this.state.guessList, guess});
+    }
+
+    render() {
+        return ('something');
+    }
+}
